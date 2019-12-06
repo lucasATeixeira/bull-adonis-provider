1fb9628b86aa0d56c8c73a601a234e0ab7cb5f2b
Lucas Teixeira <lucas.teixeira@realcf.online>
2019-12-06 18:09:22 -0300

    Sending mail

--- app/Jobs/SendMail.js
+++ app/Jobs/SendMail.js
@@ -1,22 +1,27 @@
-'use strict'
+"use strict";
+
+const Mail = use("Mail");
 
 class SendMail {
-
   // Bulls options https://optimalbits.github.io/bull/
-  static get options () {
-    return {}
+  static get options() {
+    return {};
   }
 
   // This is required. This is a unique key used to identify this job.
-  static get key () {
-    return 'SendMail-job'
+  static get key() {
+    return "SendMail-job";
   }
 
   // This is where the work is done.
-  async handle ({ data }) {
-    console.log('SendMail-job started')
+  async handle({ data }) {
+    await Mail.send(["emails.test"], {}, message => {
+      message
+        .to("lucas.at.negocios@gmail.com")
+        .from("lucas.at.negocios@gmail.com")
+        .subject("Queue test");
+    });
   }
 }
 
-module.exports = SendMail
-
+module.exports = SendMail;
